c8844ae0c9bc9d142f35782671e6ea2999309b09
Oscar Cabanillas <oskbanillas@gmail.com>
2021-05-31 17:50:43 -0300

    Listo para entegrar con el FE

--- src/models/Category.js
+++ src/models/Category.js
@@ -3,8 +3,6 @@ import mongoose, { Schema } from "mongoose";
 const categorySchema = new Schema({
   nombreCategoria: {
     type: String,
-    maxlength: 25,
-    unique: true,
     require: true,
   },
 });
